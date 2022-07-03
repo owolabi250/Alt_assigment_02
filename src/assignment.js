d4fa2d5f7e7fed3d5737de45b20c89c4c97b6b87
KennyChoco <owolabikehinde250@gmail.com>
2022-07-03 14:43:34 +0100

    assigment done

--- src/assignment.js
+++ src/assignment.js
@@ -1,59 +1,77 @@
 // ========================
 // DO NOT EDIT THIS BLOCK
-const assignment = {};
+const assignment = {}
 // ========================
 
-
 /**
- * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
+ * Challenge - 1*/
+const numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+/** Calculate and return the sum of the numbers in an array.
  * If you did Challenge - 1, remove the comment in the line just after this function
- * 
+ *
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0
+  for (let i = 0; i < arrayOfNumbers.length - 1; i++) {
+    sum += i
+    console.log(sum)
+  }
+  return arrayOfNumbers.length
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+sumOfNumbers(numberArray)
 
 /**
  * Challenge - 2
- * 
+ *
  * Count all even numbers within an array of numbers.
  * If you did Challenge - 2, remove the comment in the line just after this function
- * 
+ *
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  const evenNumbers = arrayOfNumbers.filter((even) => {
+    if (even % 2 === 0) {
+      return even
+    } else {
+      return
+    }
+  })
+  console.log(evenNumbers.length)
+  return evenNumbers.length
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+countEvenNumbers(numberArray)
 
 /**
  * Challenge - 3
- * 
+ *
  * Given an array of numbers representing temperatures in Celsius,
  * convert each number to Fahrenheit and return a new array containing the converted
  * temperatures in Fahrenheit. Decimal figures in the converted values in Fahrenheit should be removed.
  * E.g 51.21 should just be 51 (hint: Math.trunc(...) function)
  * If you did Challenge - 3, remove the comment in the line just after this function
- * 
+ *
  * See https://www.thoughtco.com/celcius-to-farenheit-formula-609227 for the conversion formula
- * 
+ *
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
-}
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+  const fahArray = arrayOfNumbers.map((cel) => {
+    const fah = cel * (9 / 5) + 32
+    // console.log(fah)
+    return Math.trunc(fah)
+  })
+  console.log(fahArray)
 
+  return fahArray
+}
+celsiusToFahrenheit(numberArray)
 
 // ========================
 // DO NOT EDIT THIS BLOCK
-module.exports = assignment;
+module.exports = assignment
 // ========================
 
